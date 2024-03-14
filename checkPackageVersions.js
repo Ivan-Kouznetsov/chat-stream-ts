f93498d8116dce0ca81bec42b0b5c0694dd4101d
Ivan Kouznetsov <ivankuz@hotmail.com>
2024-03-13 23:06:48 -0700

    add hooks

---
+++ checkPackageVersions.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+
+const dependencies = Object.values(packageJson.dependencies);
+const devDependencies = Object.values(packageJson.devDependencies);
+
+const allDependencies = dependencies.concat(devDependencies);
+
+allDependencies.forEach((version) => {
+    if (version.startsWith('^')) {
+        console.error(`Version string "${version}" starts with a caret (^). Please use exact version numbers.`);
+        process.exit(1);
+    }
+});
+
+console.log('All version strings are exact.');
